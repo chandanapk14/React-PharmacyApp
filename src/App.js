5507aa446e5599f68ca8d3431b280a5dfed3decd
chandanapk14 <chandanapushkaran@gmail.com>
2024-06-13 09:14:33 +0530

    project completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Add from './components/Add';
+import Viewall from './components/Viewall';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path='/add' element={<Add/>}/>
+        <Route path='/view' element={<Viewall/>}/>
         </Routes></BrowserRouter>
     </div>
   );
